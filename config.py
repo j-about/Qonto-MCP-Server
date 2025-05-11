6d605f030c0db2749a61c32b0f9dd6d12a5ff059
j-about <142051449+j-about@users.noreply.github.com>
2025-05-11 23:40:07 +0200

    Refactored project structure: converted app to library and standardized functions

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BASE_URL="https://thirdparty.qonto.com/v2"
-API_IDENTIFIER = os.environ.get("QONTO_API_IDENTIFIER")
-API_SECRET_KEY = os.environ.get("QONTO_API_SECRET_KEY")
-API_KEY=f"{API_IDENTIFIER}:{API_SECRET_KEY}"
